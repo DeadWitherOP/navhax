54cd41adeeafb8e1f7cfed40fc0fc9babb599131
DeadWitherOP <94380539+DeadWitherOP@users.noreply.github.com>
2023-12-14 13:15:30 +0000

    Create orbital.js

---
+++ orbital.js
@@ -0,0 +1,8 @@
+//Max buffs
+chrome.storage.local.set({ '1@armour': 7 });
+chrome.storage.local.set({ '1@spin': 7 });
+chrome.storage.local.set({ '1@speed': 7 });
+chrome.storage.local.set({ '1@damage': 7 });
+
+//Weapons
+chrome.storage.local.set({ '1@w_': 5 });
